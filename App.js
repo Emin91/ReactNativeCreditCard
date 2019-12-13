0a85284c8b818c5e00f47c19fcf7b27f2bae2ae4
Emin <emin.proger@gmail.com>
2019-12-13 13:49:54 +0400

    Animated

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import React, {Component} from 'react'
-import { StyleSheet, View, TouchableOpacity, Image, Text, ScrollView} from 'react-native'
+import { StyleSheet, View, TouchableOpacity, Image, Text, TouchableWithoutFeedback, Animated} from 'react-native'
 
 export default class App extends Component{
+
+  state = {
+    animatePress: new Animated.Value(1)
+  }
+
+  animateIn(){
+    Animated.timing(this.state.animatePress,{
+      toValue:0.98,
+      duration:100,
+    }).start()
+  }
+  animateOut(){
+    Animated.timing(this.state.animatePress,{
+      toValue:1,
+      duration:200,
+    }).start()
+  }
   render(){
     return(
       <View style={styles.container}>
-        <TouchableOpacity style={styles.TouchableView}>
-          <Image source={require('./assets/image/bg2.jpg')} resizeMode="stretch" style={{width: '100%', height: '100%', borderRadius: 10,}}></Image>
+        <TouchableWithoutFeedback 
+          onPressIn={()=>this.animateIn()}
+          onPressOut={()=>this.animateOut()}
+          style={styles.TouchableView}>
+          <Animated.View style={{backgroundColor: '#456987', width: '95%', height: 250, borderRadius: 12,
+            transform: [
+              {
+                scale:this.state.animatePress
+              }
+            ]}}>
+          <Image source={require('./assets/image/bg2.jpg')} resizeMode="stretch" style={{width: '100%', height: '100%', borderRadius: 8,}}></Image>
           <Image source={require('./assets/image/chip.png')} style={styles.img}></Image>
           <View style={styles.textView}>
             <Text style={styles.bankName}>My Bank</Text>
@@ -20,7 +46,8 @@ export default class App extends Component{
             <Text style={styles.dateViewValid}>VALID</Text>
             <Text style={styles.dateViewThru}>THRU</Text>
           </View>
-        </TouchableOpacity>
+          </Animated.View>
+        </TouchableWithoutFeedback>
       </View>
     )
   }
@@ -31,6 +58,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#141414',
   },
   TouchableView: {
     backgroundColor: '#fff',
